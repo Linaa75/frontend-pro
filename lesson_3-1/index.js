df8e713d6ab0c8d3be1d444fcaca462fffc098a9
Valeria N <v.567insp@gmail.com>
2024-11-12 13:04:54 +0200

    lesson_3-1

---
+++ lesson_3-1/index.js
@@ -0,0 +1,20 @@
+const num = 5;
+console.log(num);
+
+const text = "Hello";
+console.log(text);
+
+const isTrue = true;
+console.log(isTrue);
+
+const empty = null;
+console.log(empty);
+
+let findSmth;
+console.log(findSmth);
+
+const uniqueSymbol = Symbol('description');
+console.log(uniqueSymbol);
+
+const bigNum = 10e7;
+console.log(bigNum);
