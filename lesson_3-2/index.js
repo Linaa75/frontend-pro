4d5d61f2f8dbb42cf824b4dab614dd0968401bd1
Valeria N <v.567insp@gmail.com>
2024-11-12 13:13:49 +0200

    lesson_3-2

---
+++ lesson_3-2/index.js
@@ -0,0 +1,5 @@
+const messageOne = prompt('Enter your first value');
+const messageTwo = prompt('Enter your second value');
+const messageThree = prompt('Enter your third value');
+
+console.log(messageOne, messageTwo, messageThree);
