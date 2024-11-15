8360ae388f7796f1a72a0a534b846ae10df9aa04
Valeria N <v.567insp@gmail.com>
2024-11-15 19:14:19 +0200

    fixed lesson_4-2

--- lesson_4-2/index.js
+++ lesson_4-2/index.js
@@ -1,6 +1,6 @@
 let num = prompt("Enter 3 numbers");
 
-if (num[0] === num [1] && num[1] === num[2]) {
+if (num[0] === num[1] && num[1] === num[2]) {
     alert("All numbers the same");
 } else if (num[0] === num[1] || num[0] === num[2] || num[0] === num[2]) {
     alert("Not all numbers the same");
