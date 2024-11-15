ff7f7f0335e17971cd448f325f340a85a213a123
Valeria N <v.567insp@gmail.com>
2024-11-15 16:41:43 +0200

    lesson_4-2

---
+++ lesson_4-2/index.js
@@ -0,0 +1,10 @@
+let num = prompt("Enter 3 numbers");
+
+if (num[0] === num [1] && num[1] === num[2]) {
+    alert("All numbers the same");
+} else if (num[0] === num[1] || num[0] === num[2] || num[0] === num[2]) {
+    alert("Not all numbers the same");
+} else {
+    alert("There are no identical numbers");
+}
+
