4f4b80e5179bb60edbdd87bc09638fde61c146a9
Valeria N <v.567insp@gmail.com>
2024-11-20 19:40:54 +0200

    fixed lesson_5-3

--- lesson_5-3/index.js
+++ lesson_5-3/index.js
@@ -1,9 +1,11 @@
-let N = parseInt(prompt("Enter a number"));
+const N = parseInt(prompt("Enter a number"));
 
-for (let i = 1; i <= 100; i++) {
-    if (i * i <= N) {
-        console.log(i);
-    } else {
-        console.log("the value is bigger than the entered number");
+if (!isNaN(N) && N >= 0) {
+    for (let i = 1; i <= 100; i++) {
+        if (i * i <= N) {
+            console.log(i);
+        } else {
+            console.log("the value is bigger than the entered number");
+        }
     }
 }
