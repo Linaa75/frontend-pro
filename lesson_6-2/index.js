13d1c0bf049e73368718b46463564bc418f9f982
Valeria N <v.567insp@gmail.com>
2024-11-23 20:56:59 +0200

    lesson_6-2

---
+++ lesson_6-2/index.js
@@ -0,0 +1,23 @@
+const array = [{}, 1, 2, 3, 4, 5, 6, 'string', null];
+
+function foo(array) {
+    let sum = 0;
+    let qty = 0;
+
+    for (let item of array) {
+        if (typeof item === 'number' && item !== null) {
+            sum += item;
+            qty++;
+        }
+    }
+
+    if (qty > 0) {
+        return sum / qty;
+    } else {
+        return 0;
+    }
+}
+
+const result = foo(array);
+
+console.log(result);
