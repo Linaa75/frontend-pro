706bf45dedf7c492dc8224a4165a538f1c038bfd
Valeria N <v.567insp@gmail.com>
2024-11-24 16:54:26 +0200

    improved code lesson_6-3

--- lesson_6-3/index.js
+++ lesson_6-3/index.js
@@ -3,13 +3,7 @@ const array = [1, 3, 4, 6, 2, 5, 7];
 function removeElement(array, item) {
     const index = array.indexOf(item);
 
-    if (index !== -1) {
-        array.splice(index, 1);
-    } else {
-        console.log("error");
-    }
-   
-    return array;
+    return index !== -1 ? array.splice(index, 1) : null;
 }
 
 removeElement(array, 4);
