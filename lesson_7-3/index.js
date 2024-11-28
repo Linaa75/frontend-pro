131cd6a269f437380d3dffc948478aa081079580
Valeria N <v.567insp@gmail.com>
2024-11-28 20:02:21 +0200

    added a function lesson_7-3

--- lesson_7-3/index.js
+++ lesson_7-3/index.js
@@ -1,40 +1,40 @@
-let ask = prompt("Enter a number bigger than 100.");
-let count = 0;
-
 function answer() {
-    console.log(`You entered ${ask}.`);
+    let count = 0;
+    let ask = prompt("Enter a number bigger than 100.");
+
+    while (count < 10) {
+        if (ask === null) { // перевірка на скасування
+            alert("You canceled. Try again.");
+            ask = prompt("Enter a number bigger than 100.");
+            count++;
+            continue;
+        }
+        if (ask === "") { // перевірка на пусту строку
+            alert("You didn't enter any number. Try again.");
+            ask = prompt("Enter a number bigger than 100.");
+            count++;
+            continue;
+        }
+
+        let num = Number(ask);
+
+        if (isNaN(num)) {
+            console.log(`You entered ${ask}.`);
+            break;
+        }
+        if (num <= 100) { 
+            alert("Error. Try again.");
+            ask = prompt("Enter a number bigger than 100.");
+            count++;
+        } else { 
+            console.log(`You entered ${ask}.`);
+            break;
+        }
+    }
+
+    if (count >= 10) {
+        alert('You reached the data entry limit. The program is stopped.');
+    } 
 }
 
-while (count < 10) {
-    if (ask === null) { // перевірка на скасування
-        alert("You canceled. Try again.");
-        ask = prompt("Enter a number bigger than 100.");
-        count++;
-        continue;
-    }
-    if (ask === "") { // перевірка на пусту строку
-        alert("You didn't enter any number. Try again.");
-        ask = prompt("Enter a number bigger than 100.");
-        count++;
-        continue;
-    }
-
-    let num = Number(ask);
-
-    if (isNaN(num)) {
-        answer();
-        break;
-    }
-    if (num <= 100) { 
-        alert("Error. Try again.");
-        ask = prompt("Enter a number bigger than 100.");
-        count++;
-    } else { 
-        answer();
-        break;
-    }
-}
-
-if (count >= 10) {
-    alert('You reached the data entry limit. The program is stopped.');
-} 
+answer();
