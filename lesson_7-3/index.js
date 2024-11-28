a87521d9d2db9eb564d66cd4bbc56663daf1e804
Valeria N <v.567insp@gmail.com>
2024-11-28 19:54:18 +0200

    added a function lesson_7-3

--- lesson_7-3/index.js
+++ lesson_7-3/index.js
@@ -1,31 +1,40 @@
 let ask = prompt("Enter a number bigger than 100.");
-let item = 0;
+let count = 0;
 
-while (item < 10) {
+function answer() {
+    console.log(`You entered ${ask}.`);
+}
+
+while (count < 10) {
     if (ask === null) { // перевірка на скасування
         alert("You canceled. Try again.");
         ask = prompt("Enter a number bigger than 100.");
+        count++;
         continue;
-    } 
+    }
     if (ask === "") { // перевірка на пусту строку
         alert("You didn't enter any number. Try again.");
         ask = prompt("Enter a number bigger than 100.");
+        count++;
         continue;
     }
 
-    ask = Number(ask);
-    item++;
+    let num = Number(ask);
 
-    if (isNaN(ask) || ask < 100) {
-        alert("Error. Try again.");
-        ask = prompt("Enter a number bigger than 100.");
-    } else {
-        alert(`You entered ${ask}.`);
+    if (isNaN(num)) {
+        answer();
         break;
     }
-
-    if (item === 9) {
-        alert('You reached the data entry limit. The program is stopped.');
+    if (num <= 100) { 
+        alert("Error. Try again.");
+        ask = prompt("Enter a number bigger than 100.");
+        count++;
+    } else { 
+        answer();
         break;
     }
 }
+
+if (count >= 10) {
+    alert('You reached the data entry limit. The program is stopped.');
+} 
