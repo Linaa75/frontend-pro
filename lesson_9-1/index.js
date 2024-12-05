9ce93ec465be2c1c62feededf21b94882140053f
Valeria N <v.567insp@gmail.com>
2024-12-05 09:56:27 +0200

    improved the code +added another solution variant

--- lesson_9-1/index.js
+++ lesson_9-1/index.js
@@ -8,28 +8,46 @@ function totalSales(company) {
                     if (typeof person.salary !== 'number') {
                         return false;
                     }
-
                     totalSalary += person.salary;
                 }
 
             } else if (typeof item === 'object') {
-                const result = count(item);
-                if (result === false) {
+                if (!count(item)) {
                     return false;
                 }
             }
         }
         return true;
     }
-        
     const checkResult = count(company);
-    if (checkResult === false) {
+    if (!checkResult) {
         console.log('you have mistake: one or more salaries are not numbers');
         return false;
     } 
-
     return totalSalary;
 }
 
 const total = totalSales(company);
 console.log(`The total salary is ${total}`);
+
+
+// another solution variant
+
+// function sumSalary(data) {
+//     if (Array.isArray(data)) {
+//         return data.reduce((total, item) => total + sumSalary(item), 0); 
+//     } else if (typeof data === 'object' && data !== null) {
+//         let sum = 0;
+//         for (let key in data) {
+//             sum += sumSalary(data[key]);
+//         }
+//         return sum;
+//     } else if (typeof data === 'number') {
+//         return data;
+//     } else {
+//         return 0;
+//     }    
+// }
+
+// const total = sumSalary(company);
+// console.log(`The total salary is ${total}`);
