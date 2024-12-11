d9fd202cb3cf4ef32a53e2a7b2d18660ec0169ae
Valeria N <v.567insp@gmail.com>
2024-12-11 17:04:29 +0200

    lesson_11-2

---
+++ lesson_11-2/index.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const text = document.getElementById('text');
+const button = document.getElementById('btn');
+const defaultColor = window.getComputedStyle(text).color;
+
+    button.addEventListener('click', () => {
+        if (text.style.color === "red") {
+            text.style.color = defaultColor;
+        } else {
+            text.style.color = "red";
+        }
+    });
+
+    return;
+
+});
+
+ 
