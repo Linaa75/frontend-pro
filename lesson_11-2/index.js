1c26abcc38d43d9b2ea816a5def7d1f6b8ae7acc
Valeria N <v.567insp@gmail.com>
2024-12-11 20:06:02 +0200

    improved code

--- lesson_11-2/index.js
+++ lesson_11-2/index.js
@@ -1,16 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-
 const text = document.getElementById('text');
 const button = document.getElementById('btn');
-const defaultColor = window.getComputedStyle(text).color;
+const defaultColor = text.style.color || '#fff';
 
-    button.addEventListener('click', () => {
-        if (text.style.color === "red") {
-            text.style.color = defaultColor;
-        } else {
-            text.style.color = "red";
-        }
-    });
+button.addEventListener('click', () => {
+    if (text.style.color === "red") {
+        text.style.color = defaultColor;
+    } else {
+        text.style.color = "red";
+    }
 });
 
  
