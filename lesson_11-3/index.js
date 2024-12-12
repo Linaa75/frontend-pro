757523122e63e38087fd6b1dc1d5c0568e5a61b8
Valeria N <v.567insp@gmail.com>
2024-12-12 13:03:22 +0200

    lesson_11-3

---
+++ lesson_11-3/index.js
@@ -0,0 +1,17 @@
+const images = [
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg'
+]
+
+const img = document.getElementById('image');
+
+const randomImg = Math.floor(Math.random() * images.length);
+
+img.src = images[randomImg];
+img.style.width = '600px';
+img.style.height = '600px';
