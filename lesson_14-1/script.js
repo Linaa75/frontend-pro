0377a0c059643c56eb550c7765336a6e406dc97a
Valeria N <v.567insp@gmail.com>
2024-12-23 14:40:49 +0200

    lesson_14-1

---
+++ lesson_14-1/script.js
@@ -0,0 +1,25 @@
+const myCarousel = document.getElementById('carouselExampleCaptions')
+const next = document.getElementById('next');
+const prev = document.querySelector('.carousel-control-prev');
+
+function checkBtns(currentIndex) {
+  const totalSlides = myCarousel.querySelectorAll('.carousel-item').length;
+  (currentIndex === 0) ? prev.style.display = 'none' : prev.style.display = 'block';
+  (currentIndex === totalSlides - 1) ? next.style.display = 'none' : next.style.display = 'block';
+};
+
+myCarousel.addEventListener('slide.bs.carousel', function(event) {
+  const currentIndex = event.to;
+  checkBtns(currentIndex);
+});
+
+const activeSlide = myCarousel.querySelector('.carousel-item.active');
+const activeIndex = Array.from(myCarousel.querySelectorAll('.carousel-item')).indexOf(activeSlide);
+checkBtns(activeIndex);
+
+const carousel = new bootstrap.Carousel(myCarousel, {
+    loop: false,
+});
+
+
+
