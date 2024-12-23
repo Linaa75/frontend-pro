a004f1a74ac2f77535dad584b77151a1aa2576c5
Valeria N <v.567insp@gmail.com>
2024-12-23 14:50:22 +0200

    changed the way to knock to btn

--- lesson_12-2/index.js
+++ lesson_12-2/index.js
@@ -2,10 +2,11 @@ function div() {
     const container = document.getElementById('btn-container');
 
     container.onclick = (event) => {
-        if (event.target.tagName === 'BUTTON') {
+        if (event.target.closest('button')) {
             alert(`The button is clicked: ${event.target.textContent}`);
         }
     }
 }
 
 div();
+
