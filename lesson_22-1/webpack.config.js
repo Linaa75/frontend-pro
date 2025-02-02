8f43d26058f537dc5395760384c77281edfd84f4
Valeria N <v.567insp@gmail.com>
2025-02-02 23:12:15 +0200

    webpack lesson

--- lesson_22-1/webpack.config.js
+++ lesson_22-1/webpack.config.js
@@ -1,4 +1,4 @@
-import { dirname, resolve } from "path";
+import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import CopyPlugin from "copy-webpack-plugin";
@@ -8,94 +8,95 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default (env, argv) => {
-    const mode = process.env.MODE || argv.mode || "development";
-    const isProd = mode === "production";  
+  const mode = process.env.MODE || argv.mode || "development";
+  const isProd = mode === "production";
 
-    return {
-        mode,
-        entry: resolve(__dirname, "src", "app.js"),
-        devtool: !isProd ? "source-map" : false,
-        output: {
-            path: resolve(__dirname, "dist"),
-            filename: "[name].[contenthash].js",
+  return {
+    mode,
+    entry: resolve(__dirname, "src", "app.js"),
+    devtool: !isProd ? "source-map" : false,
+    output: {
+      path: resolve(__dirname, "dist"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-transform-runtime"],
+            },
+          },
         },
-        module: {
-            rules: [
-                {
-                    test: /\.(?:js|mjs|cjs)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                        presets: ["@babel/preset-env"],
-                        plugins: ["@babel/plugin-transform-runtime"],
-                        },
-                    },
-                },
-                {
-                    test: /\.css$/,
-                    exclude: /node_modules/,
-                    use: [
-                        "style-loader",
-                        {
-                            loader: "css-loader",
-                            options: {
-                            sourceMap: !isProd,
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /\.s[ac]ss$/i,
-                    exclude: /node_modules/,
-                    use: [
-                        "style-loader",
-                        {
-                            loader: "css-loader",
-                            options: {
-                            sourceMap: true,
-                            },
-                        },
-                        {
-                            loader: "sass-loader",
-                            options: {
-                            sassOptions: {
-                                style: `compressed`,
-                                sourceMap: true,
-                                quietDeps: true,
-                                },
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /\.(jpe?g|png|gif|svg)$/,
-                    type: "asset",
-                  },
-                ],
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
             },
-            optimization: {
-                chunkIds: "named",
-                splitChunks: {
-                chunks: "all",
-                },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /node_modules/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
             },
-            plugins: [
-                new HtmlWebpackPlugin({
-                title: "Webpack lesson_22",
-                scriptLoading: "defer",
-                minify: true,
-                template: resolve(__dirname, "src", "view", "index.html"),
-                }),
-            new CopyPlugin({
-                patterns: [
-                {
-                    from: resolve(__dirname, "src", "public"),
-                    to: resolve(__dirname, "dist"),
+            {
+              loader: "sass-loader",
+              options: {
+                sassOptions: {
+                  style: `compressed`,
+                  sourceMap: true,
+                  quietDeps: true,
                 },
-                ],
-            }),
-            new ESLintPlugin(),
-            ],
-    };
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/,
+          type: "asset",
+        },
+      ],
+    },
+    optimization: {
+      chunkIds: "named",
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack lesson",
+        scriptLoading: "defer",
+        minify: true,
+        template: resolve(__dirname, "src", "view", "index.html"),
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: resolve(__dirname, "src", "public"),
+            to: resolve(__dirname, "dist"),
+          },
+        ],
+      }),
+      new ESLintPlugin(),
+    ],
+  };
 };
